ea111df6423f4215cfa21c429eea71d0be904fcd
Davina Armstrong <davina@playableworlds.com>
2021-04-02 10:20:24 -0700

    Bump up version

--- Runtime/Network/G_NetworkMonitor.cs
+++ Runtime/Network/G_NetworkMonitor.cs
@@ -1,18 +1,17 @@
-/* --------------------------------------------------------------
- * Author:          Davina Armstrong (davina@playableworlds.com)
- * --------------------------------------------------------------*/
+/* -------------------------------------------------------------- *
+ * Author:          Davina Armstrong (davina@playableworlds.com)  *
+ * ---------------------------------------------------------------*/
 
 using UnityEngine;
 using UnityEngine.Profiling;
 
 namespace Tayx.Graphy.Network
 {
-    public class G_NetworkMonitor: MonoBehaviour
+    public class G_NetworkMonitor : MonoBehaviour
     {
         #region Variables -> Private
 
-        private int m_bytesReceived = 0;
-        private const string m_dllName = "network.bindings.dll";
+        public int m_bytesReceived { get;  set; }  = 0;
 
         #endregion
 
@@ -26,8 +25,6 @@ namespace Tayx.Graphy.Network
 
         private void Update()
         {
-            Bootstrap.ClientWorld.GetExistingSystem<ApathyTransportClientSystem>();
-            m_bytesReceived = network_recv()
         }
 
         #endregion
